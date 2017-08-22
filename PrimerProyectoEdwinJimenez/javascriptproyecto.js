59b1a824aeb4c2c78242d331881bc80fb37cdf1c
Edwin <ejimenezu@est.utn.ac.cr>
2017-08-21 18:39:37 -0600

    Se crea el login y se muestra el usuario logueado

--- PrimerProyectoEdwinJimenez/javascriptproyecto.js
+++ PrimerProyectoEdwinJimenez/javascriptproyecto.js
@@ -24,57 +24,6 @@ users= [];
      });
 
 });
-//actualizar localstorage
-$(document).ready(function(){    
-    
-    $('#btnSave').click(function(){        
-        var user = {
-               
-                name: $('#name').val(),
-                lastname: $('#lastname').val(),
-                phone: $('#phone').val(),
-                username: $('#username').val(),
-                password: $('#password').val(),
-             
-               
-          };
-var users = JSON.parse(localStorage.getItem('users'));
-if(!users){
-users= [];
-
-}
-         users.push(user);
-        localStorage.setItem('users', JSON.stringify(users));
-  
-     
-     });
-
-});
-//Guarda el usuario en sessionstorage
-$(document).ready(function(){    
-    
-    $('#btnSave').click(function(){        
-    var user = {
-                 name: $('#name').val(),
-                lastname: $('#lastname').val(),
-                phone: $('#phone').val(),
-                username: $('#username').val(),
-                password: $('#password').val(),
-               
-          };
-var users = JSON.parse(sessionStorage.getItem('users'));
-if(!users){
-users= [];
-
-}
-         users.push(user);
-        sessionStorage.setItem('users', JSON.stringify(users));
-      
-     
-     });
-
-});
-
 
 
 //Guarda el ride en localstorage
@@ -94,7 +43,7 @@ $(document).ready(function(){
   for (var i = 0; i < users.length; i++) {
     var u = users[i];
 
-    nombreUsu=u.name;
+    nombreUsu=u.username;
     console.log(nombreUsu);
     //user_html = user_html +u.name;
   }
@@ -102,7 +51,7 @@ $(document).ready(function(){
         var ride = {
               
 
-                nombre:u.name,
+                nombre:u.username,
                 name: $('#name').val(),
                 start: $('#start').val(),
                 end: $('#end').val(),
@@ -152,6 +101,28 @@ rides= [];
 
 });
 
+//Guarda el usuario en sessionstorage
+$(document).ready(function(){    
+    
+    $('#btnlog').click(function(){        
+    var user = {
+         
+                username: $('#username').val(),
+                password: $('#password').val(),
+               
+          };
+var users = JSON.parse(sessionStorage.getItem('users'));
+if(!users){
+users= [];
+
+}
+         users.push(user);
+        sessionStorage.setItem('users', JSON.stringify(users));
+      
+     
+     });
+
+});
 
 function loadSession() {
 
@@ -165,7 +136,7 @@ users=[];
     var u = users[i];
 
 
-    user_html = user_html +u.name;
+    user_html = user_html +u.username;
     
   }
 
@@ -188,12 +159,10 @@ var users = JSON.parse(sessionStorage.getItem('users'));
   for (var i = 0; i < users.length; i++) {
     var u = users[i];
 
-    nombreUsu=u.name;
+    nombreUsu=u.username;
     
     //user_html = user_html +u.name;
   }
-
-
 
 
   var rides = JSON.parse(localStorage.getItem('rides'));
@@ -209,9 +178,6 @@ rides=[];
 
      ride_html = ride_html+r.nombre  +"<td><td>" +r.name + "<td><td>"+r.start + "<td><td>"+r.end + "<td><td>"+r.description + "<td><td>"+r.departure + "<td><td>"+r.estimate + "<td><td>"+r.days;
 
-    }else{
-      alert("No sirvio :(")
-    }
     
   }
 
@@ -220,7 +186,41 @@ rides=[];
 }
 
 
+$(document).ready(function(){    
 
-//function deshabilitar(){
-//document.boti.btnSave.disabled=true;
-//}
+   $('#btnlog').click(function(){   
+
+
+var users = JSON.parse(localStorage.getItem('users'));
+   if(!users){
+   users=[];
+   alert("no sirve");
+}
+
+  var nombreUsu="";
+
+  for (var i = 0; i < users.length; i++) {
+    var u = users[i];
+
+    nombreUsu=u.username;
+    pass=u.password;
+    var usernameIntro=document.getElementById('username').value;
+    var passwordIntro=document.getElementById('password').value;
+
+    console.log(usernameIntro);
+
+    if (usernameIntro==nombreUsu&&passwordIntro==pass) {
+      alert("Estas Logueado");
+      window.location.replace("file:///C:/Users/Alfredo/Documents/GitHub/ProyectoUnoEdwinJimenez/ProyectoUnoEdwinJimenez/PrimerProyectoEdwinJimenez/CrearRide.html");
+    errorElement.setAttribute("style","display:none;");
+    }
+    
+       } 
+
+ });
+
+});
+
+
+
+
