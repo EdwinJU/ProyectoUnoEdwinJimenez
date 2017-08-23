3b3886a4da8df8f61066537aa2b10425b3f07b4d
Edwin <ejimenezu@est.utn.ac.cr>
2017-08-23 13:26:45 -0600

    Se editan los rides

--- PrimerProyectoEdwinJimenez/javascriptproyecto.js
+++ PrimerProyectoEdwinJimenez/javascriptproyecto.js
@@ -32,22 +32,32 @@ $(document).ready(function(){
    $('#btnSaveR').click(function(){   
 
   var users = JSON.parse(sessionStorage.getItem('users'));
+  var usersD= JSON.parse(localStorage.getItem('users'));
    if(!users){
    users=[];
-   alert("no sirve");
+   alert("Debes loguearte o registrarte primero");
 }
 
-  var user_html = "";
+  var localUsuName = "";
   var nombreUsu="";
+
+  for (var i = 0; i < usersD.length; i++) {
+    var ul = usersD[i];
+
+    localUsuName=ul.username;
+    break;
+  }
 
   for (var i = 0; i < users.length; i++) {
     var u = users[i];
 
     nombreUsu=u.username;
     console.log(nombreUsu);
-    //user_html = user_html +u.name;
-  }
-    
+    break;
+      }
+
+      if (nombreUsu==localUsuName) {
+
         var ride = {
               
 
@@ -68,6 +78,12 @@ rides= [];
 }
          rides.push(ride);
         localStorage.setItem('rides', JSON.stringify(rides));
+
+      }else{
+        alert("El usuario Logueado no existe");
+      }
+    
+        
   
      
      });
@@ -124,6 +140,7 @@ users= [];
 
 });
 
+//Visualizar la persona logueada
 function loadSession() {
 
   var users = JSON.parse(sessionStorage.getItem('users'));
@@ -143,14 +160,14 @@ users=[];
   $('#user_table').html(user_html);
 
 }
-
+//Mostrar rides creados por la persona logueada
 function loadRides() {
 
 
 var users = JSON.parse(sessionStorage.getItem('users'));
    if(!users){
    users=[];
-   alert("no sirve");
+   alert("Debes loguearte o registrarte primero");
 }
 
   var user_html = "";
@@ -176,7 +193,7 @@ rides=[];
 
      if(r.nombre==nombreUsu){
 
-     ride_html = ride_html+r.nombre  +"<td><td>" +r.name + "<td><td>"+r.start + "<td><td>"+r.end + "<td><td>"+r.description + "<td><td>"+r.departure + "<td><td>"+r.estimate + "<td><td>"+r.days;
+     ride_html = ride_html+"<td><td>" +r.name + "<td><td>"+r.start + "<td><td>"+r.end + "<td><td>"+r.description + "<td><td>"+r.departure + "<td><td>"+r.estimate + "<td><td>"+r.days;
 
     
   }
@@ -186,7 +203,7 @@ rides=[];
 }
 }
 
-
+//Login
 $(document).ready(function(){    
 
    $('#btnlog').click(function(){   
@@ -195,7 +212,7 @@ $(document).ready(function(){
 var users = JSON.parse(localStorage.getItem('users'));
    if(!users){
    users=[];
-   alert("no sirve");
+   alert("no ha usuarios");
 }
 
   var nombreUsu="";
@@ -213,15 +230,25 @@ var users = JSON.parse(localStorage.getItem('users'));
 
     if (usernameIntro==nombreUsu&&passwordIntro==pass) {
       alert("Estas Logueado");
-      window.location.replace("file:///C:/Users/Alfredo/Documents/GitHub/ProyectoUnoEdwinJimenez/ProyectoUnoEdwinJimenez/PrimerProyectoEdwinJimenez/CrearRide.html");
-    //errorElement.setAttribute("style","display:none;");
+      window.location.replace("file:///C:/Users/Alfredo/Documents/GitHub/ProyectoUnoEdwinJimenez/ProyectoUnoEdwinJimenez/PrimerProyectoEdwinJimenez/dashboard.html");
+
+    break;
+    }else if (usernameIntro!=nombreUsu&&passwordIntro!=pass) {
+
+      alert("El usuario no existe");
+      break;
+
     }
+      
+      
+      
+    
     
        } 
  });
 });
 
-
+//Buscar Ride para la pantalla de home
 function verRides() {
 
   var rides = JSON.parse(localStorage.getItem('rides'));
@@ -251,3 +278,154 @@ alert("No hay rides")
 }
 
 
+
+
+
+
+
+
+$(document).ready(function(){    
+
+   $('#btnUp').click(function(){ 
+    var nameIntro = document.getElementById('name').value;
+    var usernameIntro = document.getElementById('username').value;
+    var lastnameIntro = document.getElementById('lastname').value;
+    var phoneIntro = document.getElementById('phone').value;
+    var passwordIntro = document.getElementById('password').value;
+
+    if (nameIntro == "" || usernameIntro == "" || lastnameIntro == "" || passwordIntro == "" || phoneIntro=="") {
+        alert("escribe todos los datos por favor");
+    }
+    else {
+        var datos = JSON.parse(localStorage.getItem("users"));
+
+
+
+        for (var i = 0; i < datos.length; i++) {
+            if (nameIntro == datos[i].name) {
+                datos[i].name = nameIntro;
+                datos[i].password = passwordIntro;
+                datos[i].lastname = lastnameIntro;
+                datos[i].phone = phoneIntro;
+                datos[i].username=usernameIntro;
+                localStorage.setItem("users", JSON.stringify(datos));
+                console.log("Sirvio")
+                break;
+            }
+        }
+
+    }
+ });
+});
+
+
+
+
+//Editar Rides
+$(document).ready(function(){    
+
+   $('#btnSaveEdit').click(function(){ 
+    var nameIntro = document.getElementById('nameEdit').value;
+    var startIntro = document.getElementById('startEdit').value;
+    var endIntro = document.getElementById('endEdit').value;
+    var departureIntro = document.getElementById('departureEdit').value;
+    var estimateIntro = document.getElementById('estimateEdit').value;
+    var descriptionIntro = document.getElementById('commentEdit').value;
+    var daysIntro = document.getElementById('daysEdit').value;
+    
+
+    if (nameIntro == "" || startIntro == "" || endIntro == "" || departureIntro == "" || estimateIntro==""
+     || descriptionIntro== "" || daysIntro == "") {
+        alert("escribe todos los datos por favor");
+    }
+    else {
+        var datosEdit = JSON.parse(localStorage.getItem("rides"));
+
+
+        for (var i = 0; i < datosEdit.length; i++) {
+            if (nameIntro == datosEdit[i].name) {
+                datosEdit[i].name = nameIntro;
+                datosEdit[i].start = startIntro;
+                datosEdit[i].end = endIntro;
+                datosEdit[i].departure = departureIntro;
+                datosEdit[i].estimate=estimateIntro;
+                datosEdit[i].description = descriptionIntro;
+                datosEdit[i].days=daysIntro;
+                localStorage.setItem("rides", JSON.stringify(datosEdit));
+                console.log("Sirvio")
+                
+            }else{
+              alert("Debes loguearte o registrarte primero");
+              break;
+            }
+        }
+
+    }
+ });
+});
+
+
+
+
+
+$(document).ready(function(){    
+
+   $('#btnSaveSett').click(function(){   
+
+  var users = JSON.parse(sessionStorage.getItem('users'));
+  var usersLo=JSON.parse(localStorage.getItem('users'));
+   if(!users){
+   users=[];
+   alert("Debes loguearte o registrarte primero");
+}
+
+  var nombreUsu="";
+  var usuLocal="";
+
+  for (var i = 0; i < usersLo.length; i++) {
+    var ul = usersLo[i];
+
+    usuLocal=ul.username;
+    break;
+  }
+
+  for (var i = 0; i < users.length; i++) {
+    var u = users[i];
+
+    nombreUsu=u.username;
+    console.log(nombreUsu);
+    break;
+  }
+
+  if (nombreUsu==usuLocal) {
+
+    var extraInfo = {
+              
+
+                nombre:u.username,
+                fullname: $('#fullname').val(),
+                speedaverage: $('#speed').val(),
+                aboutme: $('#comment').val(),
+                      
+               
+          };
+var extraInfos = JSON.parse(localStorage.getItem('extraInfos'));
+if(!extraInfos){
+extraInfos= [];
+
+}
+         extraInfos.push(extraInfo);
+        localStorage.setItem('extraInfos', JSON.stringify(extraInfos));
+
+  }else{
+    alert("El Usuario no existe");
+  }
+
+    
+        
+  
+     
+     });
+  
+
+});
